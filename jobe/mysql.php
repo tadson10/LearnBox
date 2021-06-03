355a6f5abf78c27090a3598c044500919a59f185
Tadej Stanonik <ts6103@student.uni-lj.si>
2021-06-03 18:19:07 +0200

    Fixed mysql.php script

--- jobe/mysql.php
+++ jobe/mysql.php
@@ -32,27 +32,41 @@ $mysqli->query("CREATE TABLE IF NOT EXISTS `keys` (
        PRIMARY KEY (`id`)
    ) ENGINE=InnoDB DEFAULT CHARSET=utf8");
 
-$mysqli->query("DELIMITER @@
-                CREATE TRIGGER `before_keys_insert` 
-                BEFORE INSERT ON jobe.keys
-                  FOR EACH ROW 
-                    IF (NEW.user_id IS NULL) THEN 
-                      SET NEW.user_id = 123456; 
-                    END IF; @@
-                DELIMITER ;");
+$result = $mysqli->query("DROP TRIGGER IF EXISTS before_keys_insert");
+
+if ($result === TRUE) {
+  echo "Trigger deleted! \r\n";
+} else {
+  echo "Error deleting trigger: " . $mysqli->error . ` \n`;
+}
+
+$result = $mysqli->query("CREATE TRIGGER before_keys_insert BEFORE INSERT ON jobe.keys
+                            FOR EACH ROW
+                            BEGIN
+                              IF (NEW.user_id IS NULL) THEN 
+                                SET NEW.user_id = 123456; 
+                              END IF; 
+                              SET NEW.level = 1;
+                              SET NEW.date_created = UNIX_TIMESTAMP();
+                            END;");
+
+if ($result === TRUE) {
+  echo "Trigger created! \r\n";
+} else {
+  echo "Error creating trigger: " . $mysqli->error . ` \r\n`;
+}
 
 // INSERT sample api key for testing
-$count = $mysqli->query("SELECT COUNT(1) AS NUM FROM jobe.keys WHERE keys.key = 'dcc9a835-9750-4725-af5b-2c839908f71'");
+$sql = "DELETE FROM jobe.keys WHERE keys.key = 'dcc9a835-9750-4725-af5b-2c839908f71'";
+$mysqli->query($sql);
 
-if ($count->fetch_assoc()["NUM"] == 0) {
-  $sql = "INSERT INTO jobe.keys (`key`, `level`, `date_created`) VALUES ('dcc9a835-9750-4725-af5b-2c839908f71', 1, UNIX_TIMESTAMP())";
-  $insert = $mysqli->query($sql);
+$sql = "INSERT INTO jobe.keys (`key`) VALUES ('dcc9a835-9750-4725-af5b-2c839908f71')";
+$insert = $mysqli->query($sql);
 
-  if ($insert === TRUE) {
-    echo "Sample API key inserted";
-  } else {
-    echo "Error: " . $sql . "<br>" . $mysqli->error . ` \n`;
-  }
+if ($insert === TRUE) {
+  echo "Sample API key inserted! \r\n";
+} else {
+  echo "Error: " . $sql . "<br>" . $mysqli->error . ` \r\n`;
 }
 
 // CREATE "logs" table
@@ -71,7 +85,7 @@ $mysqli->query("CREATE TABLE IF NOT EXISTS `logs` (
    ) ENGINE=InnoDB DEFAULT CHARSET=utf8");
 
 // CREATE "limits" table
-$mysqli->query("CREATE TABLE `limits` (
+$mysqli->query("CREATE TABLE IF NOT EXISTS `limits` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `uri` VARCHAR(255) NOT NULL,
   `count` INT(10) NOT NULL,
